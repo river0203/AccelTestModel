da62b9e86d8653dbc4a7e74c44e4da1ad07558d7
river0203 <aspp3666@gmail.com>
2024-08-07 13:40:49 +0900

    update panel

--- SuddenAccel/SuddenAccelModel.java
+++ SuddenAccel/SuddenAccelModel.java
@@ -1,2 +1,10 @@
 public class SuddenAccelModel {
+    public float accelPressure;
+    public float moveRange;
+    public float moveTimer;
+
+    public SuddenAccelModel()
+    {
+
+    }
 }
--- SuddenAccel/SuddenAccelView.java
+++ SuddenAccel/SuddenAccelView.java
@@ -1,2 +1,14 @@
-public class SuddenAccelView {
+import javax.swing.*;
+import java.awt.*;
+
+public class SuddenAccelView extends JPanel{
+    private JPanel       presserBarPanel, speedBarPanel;
+    private JProgressBar presserBar, speedBar;
+
+    public SuddenAccelView()
+    {
+        setBackground(Color.white);
+        setPreferredSize(new Dimension(600, 800));
+        setLayout(null);
+    }
 }
